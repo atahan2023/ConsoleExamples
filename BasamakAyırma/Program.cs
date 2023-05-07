89b3d30736eebfa68e3b88b744f8e93040aa0e9d
atahan2023 <130838663+atahan2023@users.noreply.github.com>
2023-05-07 18:25:12 +0300

    Add files via upload

---
+++ BasamakAyırma/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+{
+    int sayi, yuzler, onlar, birler;
+    Console.Write("SAYIYI GİRİN");
+    sayi = Convert.ToInt32(Console.ReadLine());
+    yuzler = sayi / 100;
+    sayi = sayi - yuzler * 100;
+    onlar = sayi / 10;
+    sayi = sayi - onlar * 10;
+    birler = sayi;
+    Console.WriteLine("Yüzler Basamağı : {0}", yuzler);
+    Console.WriteLine("Onlar Basamağı : {0}", onlar);
+    Console.WriteLine("Birler Basamağı : {0}", birler);
+    Console.ReadKey();
+}
