f86b061258da053bd1013803af5d9f8ee3b937f8
atahan2023 <130838663+atahan2023@users.noreply.github.com>
2023-05-08 09:20:10 +0300

    Add files via upload

---
+++ İkiSayıToplamı/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int sayi1, sayi2, toplam;
+Console.Write("1. Sayı : ");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("2. Sayı : ");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+toplam = sayi1 + sayi2;
+Console.WriteLine("{0} ve {1} Sayısının Toplamı : {2}", sayi1, sayi2, toplam);
+Console.ReadKey();
